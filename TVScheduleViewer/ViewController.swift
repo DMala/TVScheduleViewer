a2df7e73da7682615ab43ea77441fede402b117b
Wendy-Anne Malaguti <WendyMalaguti@marvintemacbook.fios-router.home>
2017-12-12 11:27:32 -0500

    Initial Commit

---
+++ TVScheduleViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVScheduleViewer
+//
+//  Created by Wendy-Anne Malaguti on 12/12/17.
+//  Copyright © 2017 Watch City Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
